486bd53343ff07346f06fd869bafc2f24eedf964
xiaopowanyi <1141101853@qq.com>
2023-11-21 06:35:58 +0000

    fix

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ while res.status_code != 200:
 text = res.text
 match = re.search(r'\[Rule\](.*?)\[URL Rewrite\]', text, re.DOTALL)
 if match:
-    result = match.group(1).strip()
+    result = match.group(1).strip().replace(',PROXY', '').replace('FINAL,direct', '')
     with open("ban.list", "w", encoding="utf-8") as file:
         file.write(result)
     print("内容已写入文件 ban.list")
